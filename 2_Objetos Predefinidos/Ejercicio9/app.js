3b15389c31f8bbd8a1d3ed7991471a453d9c3197
Cesferort <cesferort99@gmail.com>
2021-09-23 14:50:51 +0200

    [T2] EJ9 - Terminado
    - EJ9 Terminado

--- 2_Objetos Predefinidos/Ejercicio9/app.js
+++ 2_Objetos Predefinidos/Ejercicio9/app.js
@@ -1,21 +1,25 @@
 let centerY = window.screen.height/2;
 let centerX = window.screen.width/2;
+let nuevaVentana;
 
 function comprobarForm(input)
 {
-    let nuevaVentana;
-
-    // Vacío
     if(input=="")          
         alert('No se ha introducido un nombre y apellido.');
     else
     {
-        nuevaVentana = window.open('','width=300 height=300','top="'+centerY+'"','left="'+centerX+'"');
-        
+        nuevaVentana = window.open('','_blank','width=300', 'height=300','top="'+centerY+'"','left="'+centerX+'"');
         nuevaVentana.document.write('<p>Bienvenido '+input+'</p>');
-        let arg='<button type="button" onClick=window.close()>Empezar a navegar</button>';
-        nuevaVentana.document.write(arg);
-
-        // TODO Cerrar nueva ventana y hacer cambios en primera ventana
+        
+        nuevaVentana.document.write
+        (
+            "<button type='button' onClick='window.opener.empezarNavegar()'>Empezar a navegar</button>"
+        );
     }
+}
+
+function empezarNavegar()
+{
+    nuevaVentana.close();
+    window.location.href = "https://www.google.es";
 }
