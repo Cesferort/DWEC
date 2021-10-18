69055c791bcda24c2f847b4f304f445383462aba
Cesferort <cesferort99@gmail.com>
2021-10-18 22:40:10 +0200

    [T7] EJ1 - Terminado
    -EJ1 Terminado

--- 7_Eventos/Ejercicio1/app.js
+++ 7_Eventos/Ejercicio1/app.js
@@ -1,26 +1,45 @@
-let listaOpciones=[];
+let lista_eleP_contenidos=[];
+let lista_eleA_enlaces=[];
 
-window.onload=function()
+window.onload
 {
-    listaOpciones=document.getElementsByClassName("chk");
+    lista_eleP_contenidos=document.getElementsByTagName("p");
+    lista_eleA_enlaces=document.getElementsByTagName("a");
+    eventos();
 }
 
-function enviar()
+function eventos()
 {
-    let txt="";
-    for(let i=0;i<listaOpciones.length;i++)
+    for(let i=0;i<lista_eleA_enlaces.length;i++)
+        lista_eleA_enlaces[i].addEventListener("click", listener_eleA_enlace);
+}
+
+function listener_eleA_enlace()
+{
+    let n=this.id.substring(this.id.indexOf('_')+1, this.id.length);
+    let eleP_contenido=recuperar_eleP_contenido(n);
+    if(this.text.trim()=="Ocultar contenidos")
     {
-        if(listaOpciones[i].checked==true)
-            txt+="*"+listaOpciones[i].value+"\n";
+        eleP_contenido.style.display="none";
+        this.text="Mostrar contenidos";
     }
-    if(txt=="")
-        alert("No has seleccionado ninguna opción.")
     else
-        alert("Has seleccionado las opciones:\n"+txt);
+    {
+        eleP_contenido.style.display="block";
+        this.text="Ocultar contenidos";
+    }
 }
 
-function limpiar()
+function recuperar_eleP_contenido(n)
 {
-    for(let i=0;i<listaOpciones.length;i++)
-        listaOpciones[i].checked=false;
-}
+    let id_eleP_contenido="contenidos_"+n;
+    let eleP_contenido;
+    let pEncontrado=false;
+    for(let i=0;i<lista_eleP_contenidos.length&&pEncontrado==false;i++)
+    {
+        eleP_contenido=lista_eleP_contenidos[i];
+        if(id_eleP_contenido.trim()==eleP_contenido.id.trim())
+            pEncontrado=true;
+    }
+    return eleP_contenido;
+}   
